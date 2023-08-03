e0ef3064d3cb89491221c29a0d6e09efb506dc03
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-08-03 16:52:33 -0500

    Update geobattle.lua

--- geobattle.lua
+++ geobattle.lua
@@ -38,29 +38,27 @@ function start()
   land = 6
   local menuOptions = {"Next round", "Stats", "Battle"}
   local chosenOption = displayMenu(menuOptions)
-  local numchose = menuOptions[chosenOption]
-  if numchose == 1 then
+  local chose = menuOptions[chosenOption]
+  if chose == "Next round" then
     if land * 15 < troops then
       -- nothing
     else
       troops = troops + (land * 5)
     end
     sleep(2)
-  elseif numchose == 2 then
+  elseif chose == "Stats" then
     print("Troop count: " .. troops)
     print("Square miles of land: " .. land)
     print("Max troops: " .. land * 15)
     print("Empty land left: " .. freeleft)
     sleep(2)
-  elseif numchose == 3 then
+  elseif chose == "Battle" then
     print("Attacking empty land.")
     oldfree = freeleft
     freeleft = freeleft / 2
     print("Gained " .. oldfree / 2 .. " square miles of land.")
     sleep(2)
   end
-  print(numchose)
-  sleep(2)
 end
 
 while true do
